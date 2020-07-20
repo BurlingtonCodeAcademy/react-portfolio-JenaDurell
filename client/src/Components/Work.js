47339ddbc5413f7d9ffba35fe565eb148d708fcf
JenaDurell <66094197+JenaDurell@users.noreply.github.com>
2020-07-20 17:53:35 -0400

    please work

--- client/src/Components/Work.js
+++ client/src/Components/Work.js
@@ -10,25 +10,25 @@ export default function Work(props) {
            <br></br>
         </div>
             <div id="experience">
-                <p7>Currently (and most importantly), I am working full time to complete a 12 week, 600 hour intensive web development bootcamp with instruction on HTML, CSS, Javascript, React, and databases supplemented by challenging weekly projects and culminating with a capstone project.
-                    </p7>
+                <p>Currently (and most importantly), I am working full time to complete a 12 week, 600 hour intensive web development bootcamp with instruction on HTML, CSS, Javascript, React, and databases supplemented by challenging weekly projects and culminating with a capstone project.
+                    </p>
                 <br></br>
                 <br></br>
                 <div id="code-skills-chart">
-                    <p7>
+                    <p>
                         Languages&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  React | JavaScript | HTML | CSS <br></br>
                         Frameworks &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;            jQuery <br></br>
                         Development&nbsp;&nbsp;&nbsp;&nbsp; Visual Studio <br></br>
                         Photo Editing &nbsp;&nbsp;&nbsp;&nbsp;Adobe Lightroom | Photoshop <br></br>
                         Source Control&nbsp;&nbsp;  Git | Github
-                    </p7>
+                    </p>
                     <br></br>
                     
                     
                 </div>
 
                 <div id = "other-work">
-                    <p7>
+                    <p>
                     Prior to jumping into the wide world of coding, I worked for over 13 years in the largest health care system in the area, providing both in and out-patient healthcare for patients from VT, NH, and Northern New York.
                     <br></br>
                     <br></br>
@@ -37,7 +37,7 @@ export default function Work(props) {
                     <br></br>
                     With an early and solid background in the service industry, I built the abilities to prioritize during stressful situations, put out fires before they start (and after they had started too, if necessary) and meet crushing and unrealistic deadlines and demands no matter what. 
 
-                    </p7>
+                    </p>
                 </div>
                 <div id = "finally">
                     <p>For a detailed description of my work history, please see my <a href = 'https://www.linkedin.com/in/jena-durell-b5044b89/' target="_blank">linkedIn profile. </a> 
