f6304e674148f091049519e14877f58e9f549b6a
JenaDurell <66094197+JenaDurell@users.noreply.github.com>
2020-07-23 20:35:08 -0400

    css detail july23

--- client/src/Components/Contact.js
+++ client/src/Components/Contact.js
@@ -3,42 +3,31 @@ import { Link } from '@reach/router'
 import '../Style/Contact.css'
 
 export default function Contact(props) {
-    
+
     return (
-        <div id = "contact-body">
-       <div id= "contact-head">
-           Let's Make Things Happen...
-       
-<br></br>
-<br></br>
-<a href = 'https://www.jpdgallery.com/' target="_blank">jpdgallery.com</a>
-<br></br>
-<br></br>
+        <div id="contact-body">
+            <div id="contact-head">
+                Let's Make Things Happen...
+           </div>
 
+            <div id="link-container">
+                <a href='https://www.jpdgallery.com/' target="_blank">jpdgallery.com</a>
 
-<a href = 'https://www.linkedin.com/in/jena-durell-b5044b89/' target="_blank">Linkedin</a>
-<br></br>
-<br></br>
-<a href = 'https://www.instagram.com/snowy_vt/?hl=en' target="_blank">Instagram</a>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
+                <a href='https://www.linkedin.com/in/jena-durell-b5044b89/' target="_blank">Linkedin</a>
 
-<div className = "phone-and-email">
-    <div>Jena Durell</div>
-    <div>802.391.4048</div>
-    <div>jena.durell@gmail.com</div>
+                <a href='https://www.instagram.com/snowy_vt/?hl=en' target="_blank">Instagram</a>
+            </div>
 
-</div>
-</div>
+            <div id="right-bottom-corner">
+                <div className="phone-and-email">
+                    <div>Jena Durell</div>
+                    <div>802.391.4048</div>
+                    <div>jena.durell@gmail.com</div>
 
-</div>
+                </div>
+            </div>
+
+        </div>
     )
 }
 
--- client/src/Components/Work.js
+++ client/src/Components/Work.js
@@ -3,64 +3,74 @@ import '../Style/Work.css'
 import WorkBackground from '../Images/bright.JPEG'
 var sectionStyle = {
     backgroundImage: `url(${WorkBackground})`,
-    backgroundPosition: 'center',
-    backgroundSize: 'cover',
-    backgroundRepeat: 'y',
+    // backgroundPosition: 'center',
+    backgroundSize: '100vw 100vh',
+    // backgroundRepeat: 'y',
     width: '100%',
-    height: '100%',
-    minHeight:"95vh"
+    height: '95vh',
+    // minHeight:"95vh"
 };
 export default function Work(props) {
     return (
         <div style={sectionStyle} id="work-body">
             <div id="work">
-                <br></br>
+                
            It's not where you've been, it's where you're going...
-           <br></br>
+           
             </div>
             <div id="experience">
-                <p>Currently (and most importantly), I am working full time to complete a 12 week, 600 hour intensive web development bootcamp with instruction on HTML, CSS, Javascript, React, and databases supplemented by challenging weekly projects and culminating with a capstone project.
-                    </p>
-                <br></br>
-                <br></br>
-                <div id="code-skills-chart">
-                    <p>
-                        Languages&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  React | JavaScript | HTML | CSS <br></br>
-                        Frameworks &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;            jQuery <br></br>
-                        Development&nbsp;&nbsp;&nbsp;&nbsp; Visual Studio <br></br>
-                        Photo Editing &nbsp;&nbsp;&nbsp;&nbsp;Adobe Lightroom | Photoshop <br></br>
-                        Source Control&nbsp;&nbsp;  Git | Github
-                    </p>
-                    <br></br>
-
-
+                <div id= "bootcamp-wrapper">
+                <div id = "bootcamp">
+                    Currently (and most importantly), I am working full time to complete a 12 week, 600 hour intensive web development bootcamp with instruction on HTML, CSS, Javascript, React, and databases supplemented by challenging weekly projects and culminating with a capstone project.
                 </div>
-
-                <div id="other-work">
-                    <p>
-                        Prior to jumping into the wide world of coding, I worked for over 13 years in the largest health care system in the area, providing both in and out-patient healthcare for patients from VT, NH, and Northern New York.
-                    <br></br>
-                        <br></br>
-                    My time in the health care industry taught me a lot about working with a collaborative team of various personalities to deliver best outcomes for each patient, every time. It cemented my drive to deliver my best at all times. It solidified my focus and attention to detail, and helped me build friendships that will last a lifetime.
-                    <br></br>
-                        <br></br>
-                    With an early and solid background in the service industry, I built the abilities to prioritize during stressful situations, put out fires before they start (and after they had started too, if necessary) and meet crushing and unrealistic deadlines and demands no matter what.
-
-                    </p>
                 </div>
-                <div id="finally">
-                    <p>For a detailed description of my work history, please see my <a href='https://www.linkedin.com/in/jena-durell-b5044b89/' target="_blank">linkedIn profile. </a>
-                        <a href='https://www.linkedin.com/in/jena-durell-b5044b89/' target="_blank"></a>
-                    </p>
+            <div id="code-skills-chart">
 
-                </div>
+                <ul id = "skills-left">
+                       <li> Languages </li>
+                       <li> Frameworks</li>
+                       <li>Development</li>
+                       <li>Photo Editing</li>
+                       <li>Source Control</li>
+                </ul>
 
+                <ul id = "skills-right"> 
+                    <li> React | JavaScript | HTML | CSS</li>
+                    <li> jQuery </li>
+                    <li> Visual Studio </li>
+                    <li> Adobe Lightroom | Photoshop </li>
+                    <li> Git | Github</li>
+                </ul>
+                   
+                    
 
-                <br></br>
-                <br></br>
 
             </div>
-        </div>
+
+                <div id="other-work">
+                    <div className = "work-para">
+                        Prior to jumping into the wide world of coding, I worked for over 13 years in the largest health care system in the area, providing both in and out-patient healthcare for patients from VT, NH, and Northern New York.
+                    </div>
+                    <div className ="work-para">
+                    My time in the health care industry taught me a lot about working with a collaborative team of various personalities to deliver best outcomes for each patient, every time. It cemented my drive to deliver my best at all times. It solidified my focus and attention to detail, and helped me build friendships that will last a lifetime.
+                    </div>
+                    <div className ="work-para">
+                    With an early and solid background in the service industry, I built the abilities to prioritize during stressful situations, put out fires before they start (and after they had started too, if necessary) and meet crushing and unrealistic deadlines and demands no matter what.
+                    </div>
+                   
+                </div>
+                <div id="finally">
+                  <div id = "linkedin">For a detailed description of my work history, please see my <a href='https://www.linkedin.com/in/jena-durell-b5044b89/' target="_blank">linkedIn profile. </a>
+                        <a href='https://www.linkedin.com/in/jena-durell-b5044b89/' target="_blank"></a>
+                        </div>
+
+                </div>
+
+
+                
+
+            </div>
+       </div>
 
     )
 }
